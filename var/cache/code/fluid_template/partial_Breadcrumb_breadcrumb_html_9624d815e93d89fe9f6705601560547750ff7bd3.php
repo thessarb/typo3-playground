19f408cf38b2f6ca6a136d050783621ba0267b6e
thessarb <sala@contenance.de>
2022-08-19 14:08:32 +0200

    something more

---
+++ var/cache/code/fluid_template/partial_Breadcrumb_breadcrumb_html_9624d815e93d89fe9f6705601560547750ff7bd3.php
@@ -0,0 +1,49 @@
+<?php
+
+class partial_Breadcrumb_breadcrumb_html_9624d815e93d89fe9f6705601560547750ff7bd3 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+  'bk2k' => 
+  array (
+    0 => 'BK2K\\BootstrapPackage\\ViewHelpers',
+  ),
+  'mask' => 
+  array (
+    0 => 'MASK\\Mask\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+
+return NULL;
+}
+
+
+}
+#
