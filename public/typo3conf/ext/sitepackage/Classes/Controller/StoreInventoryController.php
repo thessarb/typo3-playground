a34f92c3ac1f8f0a485ccc6d5e205568b34a57e0
thessarb <sala@contenance.de>
2022-08-30 15:24:12 +0200

    more changes

--- public/typo3conf/ext/sitepackage/Classes/Controller/StoreInventoryController.php
+++ public/typo3conf/ext/sitepackage/Classes/Controller/StoreInventoryController.php
@@ -37,4 +37,55 @@ class StoreInventoryController extends ActionController
         $products = $this->productRepository->findAll();
         $this->view->assign('products', $products);
     }
+
+    /**
+     * store Action
+     * 
+     * @return void
+     */
+    public function storeAction(Product $product)
+    {
+        $request = $_POST["tx_youtubedemo_inventorylist"]["product"]; 
+        $product->setName($request['name']);
+        $product->setDescription($request['description']);
+        $product->setQuantity($request['quantity']);
+        $this->productRepository->add($product);
+        return $this->redirect('list');
+    }
+
+    /**
+     * list Action
+     *
+     * @return void
+     */
+    public function editAction()
+    {
+        $id = $_POST["tx_youtubedemo_inventorylist"]["product"]["uid"]; 
+        $product = $this->productRepository->findByUid($id);
+        $this->view->assign('product', $product);
+    }
+
+    public function updateAction()
+    {
+        $request = $_POST["tx_youtubedemo_inventorylist"]["product"]; 
+        $product = $this->productRepository->findByUid($request['uid']);
+        $product->setName($request['name']);
+        $product->setDescription($request['description']);
+        $product->setQuantity($request['quantity']);
+        $this->productRepository->update($product);
+        return $this->redirect('list');
+    }
+
+        /**
+     * list Action
+     *
+     * @return void
+     */
+    public function deleteAction()
+    {
+        $id = $_POST["tx_youtubedemo_inventorylist"]["product"]["uid"]; 
+        $product = $this->productRepository->findByUid($id);
+        $this->productRepository->remove($product);
+        return $this->redirect('list');
+    }
 }
